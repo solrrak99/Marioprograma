1690b7fb00c19ffec79ee812c443da243489b978
solrrak <solrrakc5@gmail.com>
2017-09-12 23:08:32 -0500

    mario
    programa

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace mario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        String cad = "";
+        int n,w,y;
+        Console.WriteLine("ingrese un numero no mayor a 28:");
+        n = Convert.ToInt32(Console.ReadLine());
+        for(y=1;y<n+1;y++){
+         cad="";
+        for(w=1;w<y+1;w++){
+        cad=cad+"#";
+         }
+         Console.WriteLine(cad.PadLeft(n)); 
+     }
+        //Carlos Alejandro Villegas Nuñez
+                                
+        //Reyes Aceves Sergio
+        }
+    }
+}
